c8e96e3811ffd58e62b84dc19deea25f0c7f3715
Xiaoyu <32483897+migapipo@users.noreply.github.com>
2019-04-17 00:20:19 +1000

    'Graphic' object draw anywhere on window

---
+++ WindowGraphics/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace UnicornSoftwares
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.uxDraw = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// uxDraw
+			// 
+			this.uxDraw.Location = new System.Drawing.Point( 205, 236 );
+			this.uxDraw.Name = "uxDraw";
+			this.uxDraw.Size = new System.Drawing.Size( 75, 23 );
+			this.uxDraw.TabIndex = 0;
+			this.uxDraw.Text = "Draw";
+			this.uxDraw.UseVisualStyleBackColor = true;
+			this.uxDraw.Click += new System.EventHandler( this.uxDraw_Click );
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size( 292, 271 );
+			this.Controls.Add( this.uxDraw );
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout( false );
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button uxDraw;
+
+	}
+}
+
---
+++ WindowGraphics/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace UnicornSoftwares
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void uxDraw_Click( object sender, EventArgs e )
+		{
+			using ( WindowGraphics wg = new WindowGraphics( this ) )
+			{
+				//.....
+				wg.Graphics.DrawLine( Pens.Blue, 0, 0, 100, 100 );
+				//.....
+
+				// or if you have to call many drawing functions, here is the way to reduce
+				// your typing. This is what I always do...
+				Graphics g = wg.Graphics;
+				g.DrawString( "I am on the title bar!", new Font( "Tahoma", 10, FontStyle.Bold ), Brushes.Black, 0, 4 );
+				g.FillEllipse( Brushes.Black, this.Width - 40, this.Height - 40, 80, 80 );
+				// .... other drawing commands...
+			}
+		}
+	}
+}
